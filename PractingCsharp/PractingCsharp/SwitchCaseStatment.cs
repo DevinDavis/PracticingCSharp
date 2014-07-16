51d7cc2e97cef24998be006612fa543b4e5f51bb
Devin Davis <dndavis1111@gmail.com>
2014-07-16 01:01:06 -0700

    ClassAndOrStatmentsCreated

---
+++ PractingCsharp/PractingCsharp/SwitchCaseStatment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractingCsharp
+{
+    class SwitchCaseStatment
+    {
+        static void Main(string[] args)
+        {
+            string j = "Two";
+
+            switch (j)
+            {
+                case "Zero":
+                    Console.WriteLine("value is 0");
+                    break;
+                case "One":
+                    Console.WriteLine("value is 1");
+                    break;
+                case "Two":
+                    Console.WriteLine("value is 2");
+                    break;  
+                case "Three":
+                    Console.WriteLine("value is 3");
+                    break;
+                case "Four":
+                    Console.WriteLine("value is 4");
+                    break;
+                default:
+                    Console.WriteLine("Value not found");
+                    break;
+            }
+    }
+    }
+}
