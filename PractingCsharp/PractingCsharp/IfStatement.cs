28d654684184d20fcb3cee00a0165bb88f3fb1cf
Devin Davis <dndavis1111@gmail.com>
2014-07-15 23:51:49 -0700

    ClassIfStatementCreated

---
+++ PractingCsharp/PractingCsharp/IfStatement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractingCsharp
+{
+    class IfStatement
+    {
+        static void Main()
+        {
+            int x = 10;
+            int y = 20;
+
+            if (x < y)
+
+                Console.WriteLine(" x is less than y");
+
+            if (x > y)
+
+                Console.WriteLine("x is greater than y");
+
+            if (x == y)
+
+                Console.WriteLine("x is equal to y");
+        }
+
+    }
+}
