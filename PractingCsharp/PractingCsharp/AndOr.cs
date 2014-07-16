9bda4d6cdb33cc41ab536713f2c24fbe8f4f4504
Devin Davis <dndavis1111@gmail.com>
2014-07-16 00:45:25 -0700

    ClassAndOrAdCreated

---
+++ PractingCsharp/PractingCsharp/AndOr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractingCsharp
+{
+    class AndOr
+    {
+        static void Main(string[] args)
+        {
+            int x = -10;
+            int y = 20;
+        
+        if (x >= 0 && y >= 0)
+        {
+            Console.WriteLine("Both numbers are possitive");
+        }
+        else if (x >= 0 || y >= 0)
+        {
+            Console.WriteLine("atleast one number is positvie");
+        }   
+            else
+                Console.WriteLine("Both numbers are negative");
+        }
+    }
+}
---
+++ PractingCsharp/PractingCsharp/DoWhileLoop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractingCsharp
+{
+    class DoWhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            do
+            {
+                Console.WriteLine("x = " + x);
+                x--;
+            }   while(x < 0);
+          }
+    }
+}
